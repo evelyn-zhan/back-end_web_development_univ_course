323332223344a991465a549fbd89b00507967642
Evelyn <evelynzanpx@gmail.com>
2024-10-02 10:56:12 +0700

    update case study for week 03

--- Week 03 - Async Request/Case Study/index.js
+++ Week 03 - Async Request/Case Study/index.js
@@ -1,4 +1,8 @@
 const show = function(data) {
+    document.getElementById("loading").style.display = "none"
+    document.getElementById("image").style.display = ""
+    document.getElementById("author").style.display = ""
+
     let jsonData = JSON.parse(data)
     jsonData = jsonData[0]
     document.getElementById("image").src = jsonData.download_url
@@ -9,20 +13,25 @@ function load() {
     const link = "https://picsum.photos/v2/list?page=2&limit=1"
     const xhr = new XMLHttpRequest()
 
-    try {
-        if(!xhr) return -1
+    setTimeout(() => {
+        try {
+            if(!xhr) return -1
 
-        xhr.open("GET", link)
-        xhr.send()
+            xhr.open("GET", link)
+            xhr.send()
 
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                show(xhr.responseText)
+            xhr.onreadystatechange = function() {
+                if(xhr.readyState == 4 && xhr.status == 200) {
+                    show(xhr.responseText)
+                }
             }
+        } catch(error) {
+            console.log(error)
         }
-    } catch(error) {
-        console.log(error)
-    }
+    }, 1500)
+
+    document.getElementById("image").style.display = "none"
+    document.getElementById("author").style.display = "none"
 }
 
 load()

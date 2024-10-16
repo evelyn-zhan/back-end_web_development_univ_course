accf256aeb75d4b1e13ae0bf6daf2c3c3deeb8e7
Evelyn <evelynzanpx@gmail.com>
2024-10-16 19:12:17 +0700

    update case study for week 05

--- Week 05 - Web Server and Node JS/Case Study/index.js
+++ Week 05 - Web Server and Node JS/Case Study/index.js
@@ -1,31 +1,26 @@
-const http = require('http')
-const fs = require('fs')
-const hostname = '127.0.0.1'
-const port = 3000
-
-const server = http.createServer((req, res) => {
-    // res.statusCode = 200
-    // res.setHeader('Content-Type', 'text/html')
-
-    // const data = fs.readFileSync('index.html')
-    // res.end(data)
-
-    if (req.url === '/' || req.url === '/index.html') {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'text/html')
-        const data = fs.readFileSync('index.html', 'utf-8')
-        res.end(data)
-    } else if (req.url === '/script.js') {
-        res.statusCode = 200
-        res.setHeader('Content-Type', 'application/javascript')
-        const script = fs.readFileSync('script.js', 'utf-8')
-        res.end(script)
-    } else {
-        res.statusCode = 404
-        res.end('404 Not Found')
+async function getData() {
+    try {
+        const res = await fetch("https://picsum.photos/v2/list")
+        const data = await res.json()
+        return data
+    } catch(error) {
+        console.log(error)
     }
-})
+}
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})
+function showData(datas) {
+    const table = document.querySelector("#data")
+
+    datas.forEach(data => {
+        table.innerHTML += `
+            <tr>
+                <td><img src="${data.download_url}" alt="" width="200px" height="100px" /></td>
+                <td>${data.author}</td>
+            </tr>
+        `
+    })
+}
+
+getData()
+    .then(data => showData(data))
+    .catch(error => console.log(error))
